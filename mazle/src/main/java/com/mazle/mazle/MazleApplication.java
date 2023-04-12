a51fd460d2bf12fcf3acfde072eb40fa7b930dd3
youngDaLee <ldy0956@gmail.com>
2023-04-12 17:08:13 +0900

    project init

---
+++ mazle/src/main/java/com/mazle/mazle/MazleApplication.java
@@ -0,0 +1,13 @@
+package com.mazle.mazle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MazleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MazleApplication.class, args);
+	}
+
+}
