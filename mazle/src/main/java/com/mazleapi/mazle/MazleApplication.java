94a7b904bc517e61413874001fd972fc49fb56d2
youngDaLee <ldy0956@gmail.com>
2023-04-12 17:36:49 +0900

    init project

--- mazle/src/main/java/com/mazleapi/mazle/MazleApplication.java
+++ mazle/src/main/java/com/mazleapi/mazle/MazleApplication.java
@@ -1,4 +1,4 @@
-package com.mazle.mazle;
+package com.mazleapi.mazle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- mazle/src/test/java/com/mazleapi/mazle/MazleApplicationTests.java
+++ mazle/src/test/java/com/mazleapi/mazle/MazleApplicationTests.java
@@ -1,4 +1,4 @@
-package com.mazle.mazle;
+package com.mazleapi.mazle;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
