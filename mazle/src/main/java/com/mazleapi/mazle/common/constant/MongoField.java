7ae4c046c3e19e48fc96f2a1221f0ea5d3ecd7ea
youngDaLee <ldy0956@gmail.com>
2023-04-15 14:55:32 +0900

    :art: Recipe Controller 추가

---
+++ mazle/src/main/java/com/mazleapi/mazle/common/constant/MongoField.java
@@ -0,0 +1,15 @@
+package com.mazleapi.mazle.common.constant;
+
+public class MongoField {
+    //ID
+    public final static String ID = "_id";
+    public final static String USER_ID = "uid";
+    public final static String RECIPE_ID = "rid";
+    public final static String DRINK_ID = "did";
+    public final static String COMMENT_ID = "cid";
+    public final static String MATERIAL_ID = "mid";
+
+    // RECIPE
+    public final static String RECIPE_NAME = "rname";
+
+}
